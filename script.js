0009d8bac53c644c23b1e167c5091fabbbae4dbf
alex <beachsidewebdesignllc@gmail.com>
2023-01-18 13:51:40 -0500

    created js operator functions

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-const title = document.title;
+const add = function (a, b) {
+  return a + b;
+};
 
-console.log(title + " is online.");
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const percent = function (a, b) {
+  //a percent of b
+  return (a / 100) * b;
+};
+
+const operate = function (operator, a, b) {
+  return operator(a, b);
+};
+
+let screen = document.getElementById("screen");
+
+const allClear = function () {
+  screen.textContent = "";
+};
+
+const plusMinus = function () {};
