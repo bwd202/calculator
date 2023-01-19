cd0708e62c581b56e8c6052cbe9141bcf52fe5b0
alex <beachsidewebdesignllc@gmail.com>
2023-01-19 16:51:29 -0500

    add numberBtn event listener

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-calculator = {
+const calculator = {
   value: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   function: {
     add(a, b) {
@@ -19,4 +19,22 @@ calculator = {
   },
 };
 
-console.log(calculator.function.percent(25, 200));
+// console.log(calculator.function.percent(25, 200));
+const clearScreen = function () {
+  display.innerText = "0";
+};
+
+const acBtn = document
+  .getElementById("ac-btn")
+  .addEventListener("click", clearScreen);
+
+const display = document.getElementById("display");
+let displayValue = 0;
+
+const consoleLog = function (e) {
+  display.innerText += e.target.innerText;
+};
+
+const numberBtn = document.querySelectorAll(".number-btn").forEach((btn) => {
+  btn.addEventListener("click", consoleLog);
+});
