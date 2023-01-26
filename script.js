61ab7332d84d5b385532c8cd1ed231d81b82453d
alex <beachsidewebdesignllc@gmail.com>
2023-01-26 17:09:16 -0500

    add new code with arrays

--- script.js
+++ script.js
@@ -1,64 +1,21 @@
-// window.addEventListener("click", operate);
+let temp = [];
+let num1 = [];
+let num2 = [];
+const screen = document.getElementById("screen");
 
-// function operate(op, num1, num2) {
-//   //add EventListener to op-btn
+const numBtn = document.querySelectorAll(".number-btn");
 
-//   const opFn = function (e) {
-//     console.log(e.target.textContent);
+numBtn.forEach((btn) => btn.addEventListener("click", getNum));
 
-//     let opBtn = document
-//       .querySelectorAll(".operator-btn")
-//       .forEach((btn) => btn.addEventListener("click", opFn));
-//   };
-// }
-
-let opBtn = document.querySelectorAll(".operator-btn");
-
-opBtn.forEach((btn) => {
-  btn.addEventListener("click", op);
-  btn.addEventListener("click", num1);
-});
-
-function op(e) {
-  //   console.log(e.target.textContent);
-}
-
-//saves display value
-function num1() {
-  console.log(screen.textContent);
-  //   return screen.textContent;
-}
-
-let screen = document.getElementById("screen");
-
-let numBtn = document.querySelectorAll(".number-btn");
-
-numBtn.forEach((num) => {
-  num.addEventListener("click", dispTxt);
-  num.addEventListener("click", num2Fn);
-});
-
-function dispTxt(e) {
-  //   console.log(e.target.textContent);
-
-  //removes leading zero
-  if (screen.firstChild.nodeValue == 0) {
+function getNum(e) {
+  console.log(e.target.textContent);
+  num1.push(e.target.textContent);
+  if (screen.firstChild.textContent == 0) {
     screen.removeChild(screen.firstChild);
   }
-
   screen.appendChild(document.createTextNode(e.target.textContent));
 }
 
-//attempts to save second num on the screen
-function num2Fn(e) {
-  //   console.log("1");
-
-  if (op) {
-    screen.appendChild(document.createTextNode(e.target.textContent));
-  }
-}
-
-//clear screen
 const clearScreen = function () {
   // console.log(screen.textContent);
   screen.textContent = 0;
@@ -67,6 +24,3 @@ const clearScreen = function () {
 const acBtn = document
   .getElementById("ac-btn")
   .addEventListener("click", clearScreen);
-
-//save num2 (add this to the num-btn click event)
-function num2() {}
