53d8d158f6de676c8a3d79460b9e1a262675824d
alex <beachsidewebdesignllc@gmail.com>
2023-02-05 18:31:08 -0500

    replace 0 for '0' in getNum and getNum2 fn

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function getNum(e) {
     }
 
     if (e.target.textContent == "." && num1.length == 0) {
-      num1.push(0);
+      num1.push("0");
     }
     num1.push(e.target.textContent);
     // console.log(num1);
@@ -142,7 +142,7 @@ function getNum2(e) {
 
   if (num2.length < 9) {
     if (e.target.textContent == "." && num2.length == 0) {
-      num2.push(0);
+      num2.push("0");
     }
     num2.push(e.target.textContent);
 
@@ -178,6 +178,9 @@ function operate() {
     x = Number.parseFloat(num1.join(""));
     y = Number.parseFloat(num2.join(""));
 
+    // x = Number.parseInt(num1.join(""));
+    // y = Number.parseInt(num2.join(""));
+
     console.log(x);
     console.log(y);
 
@@ -206,7 +209,7 @@ function operate() {
     if (result > 999999999) {
       let x = result * 0.1;
       screen.textContent = x.toPrecision(1);
-    } else if (result.toString().length > 9) {
+    } else if (result.toString().length > 8) {
       //   console.log("hello");
       //   screen.style.fontSize = "40px";
       //   screen.style.paddingTop = "62px";
